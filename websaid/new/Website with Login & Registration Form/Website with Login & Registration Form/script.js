05b684cf3b1fa61dd33a99395f78a294792daac3
Majed-Al-sharif <129229941+Majed-Al-sharif@users.noreply.github.com>
2024-06-30 07:40:18 +0300

    Add files via upload

---
+++ websaid/new/Website with Login & Registration Form/Website with Login & Registration Form/script.js
@@ -0,0 +1,65 @@
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+arabic.onclick = ()=>{
+  setLanugage("arabic");
+  localStorage.setItem("Lang","arabic");
+};
+
+english.onclick = ()=>{
+  setLanugage("english");
+  localStorage.setItem("Lang","english");
+};
+
+onload = ()=>{
+  setLanugage(localStorage.getItem("Lang"));
+};
+function setLanugage(getLanuage){
+if(getLanuage === "arabic"){
+  Home.innerHTML = "الرئيسية";
+  About.innerHTML = "معلومات عنا";
+  Contact.innerHTML = "اتصل بنا";
+  Servicer.innerHTML = "خدماتنا";
+  Login1.innerHTML = "الدخول";
+}else if(getLanuage ==="english"){
+  Home.innerHTML = "Home";
+  Product.innerHTML = "Product";
+  Services.innerHTML = "Services";
+  Contact.innerHTML = "Contact";
+  Login.innerHTML = "Login";
+
+}
+
+
+}
+
